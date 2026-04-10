f13a2e1911e9d204494490752b52ffd4f686052c
Abinas Patra <2010abinas@gmail.com>
2026-04-10 16:22:55 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { createHashRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider, Navigate } from 'react-router-dom'
 import { ThemeProvider } from './context/theme'
 import HomePage from './pages/home'
 
 const router = createHashRouter([
   { path: '/', element: <HomePage /> },
+  { path: '*', element: <Navigate to="/" replace /> },
 ])
 
 export default function App() {
